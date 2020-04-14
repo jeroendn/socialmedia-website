b4d6834ef6d1af96f151aa0753522177f92f096d
jeroendn <51778824+jeroendn@users.noreply.github.com>
2020-04-14 11:43:07 +0200

    Added follow overview page

--- pages/follows.php
+++ pages/follows.php
@@ -17,7 +17,7 @@ include_once __DIR__ . '../../php/session.php';
       <section class="container mt-3">
         <?php
         try {
-          $sql = "SELECT * FROM follow WHERE user_id = '" . $_SESSION['user_id'] . "' ";
+          $sql = "SELECT * FROM follow INNER JOIN user ON follow.followed_user_id = user.id WHERE user_id = '" . $_SESSION['user_id'] . "' ";
           $stmt = $conn->prepare($sql);
           $stmt->execute();
           $user_data = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -26,8 +26,13 @@ include_once __DIR__ . '../../php/session.php';
           sql_error($e);
         }
 
-        foreach ($user_data as $user) {
-          ?><div><?php echo $user['followed_user_id']; ?></div><?php
+        foreach ($user_data as $user) {?>
+          <div class="card mb-2 pr-3 pl-3">
+            <img src="php/get_profile_icon.php?user=<?php echo htmlspecialchars($user['username']); ?>" />
+            <p><?php echo $user['username']; ?></p>
+            <?php $user_id = $user['id']; $username = $user['username']; include __DIR__ . '../../php/template_parts/follow_btn.php'; ?>
+          </div>
+        <?php
         }
         ?>
       </section>
